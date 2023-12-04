78d1d97a73586dccfe707f7ba96ad0d798b3f71b
karthik <karthik.manne@kraftheinz.com>
2023-12-04 13:40:39 +0530

    First draft

---
+++ Functions.py
@@ -0,0 +1,156 @@
+import json
+import os
+import datetime
+import langchain
+import openai
+from langchain.prompts import PromptTemplate
+from langchain.llms import AzureOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# OPENAI_API_KEY = "c9c15fe92ffc49d68f1194e1b84320ef"
+# OPENAI_DEPLOYMENT_NAME = "content"
+# MODEL_NAME = "text-davinci-003"
+#
+os.environ["OPENAI_API_TYPE"] = "azure"
+os.environ["OPENAI_API_VERSION"] = "2023-05-15"
+os.environ["AZURE_OPENAI_ENDPOINT"] = "https://cmo-dev-des-ai.openai.azure.com/"
+os.environ["OPENAI_API_KEY"] = "c9c15fe92ffc49d68f1194e1b84320ef"
+os.environ["DEPLOYMENT_NAME"] = "content"
+os.environ["MODEL_NAME"] = "gpt-4-turbo"
+
+class ContentLab():
+    def __init__(self):
+
+        self.llm = AzureOpenAI(temperature=0.0)
+        self.memory = ConversationBufferMemory()
+        self.conversation = ConversationChain(
+            llm=self.llm,
+            memory=self.memory,
+            verbose=True
+            )
+
+    def what_to_accomplish(self,inp):
+
+        # user_quest = str(json.load(inp)["user_quest"])
+        # mar_stages = json.load(inp)["stages"]
+        mar_stages = ["Awareness", "Acquisition", "Activation", "Revenue", "Retention", "Referral"]
+
+        prompt_template = PromptTemplate.from_template("Assume a role of marketer but do not acknowledge it in the response.\n \
+            Give me a brief explanation to achieve my goal: {user_question}.\n \
+            Given the list of {marketing_stages} please provide a stage I should be focusing on based on my goal.\n \
+            The output format is a dictionary with one key as text and the value as the explanation and the \
+            other key is funnel focus and the value is your recommended funnel focus.")
+        edited_prompt = prompt_template.format(user_question=inp, marketing_stages=mar_stages)
+        print("prompt ........", edited_prompt)
+
+        response = self.conversation.predict(input=edited_prompt)
+        print("response ........", response)
+
+        return response
+
+
+    def options_in_funnel_focus(self,funnel, user_question):
+
+        # user_quest = str(json.load(inp)["user_quest"])
+        # mar_stages = json.load(inp)["stages"]
+
+        prompt_template = PromptTemplate.from_template("My focus now is {funnel_focus} stage in marketing.\n \
+             Give me a list of strategies or options that I can consider to achieve {user_quest} \
+             in my current focused stage?\n Give me just the option names. Do not provide any explanation or \
+             introductions for the list of options.\n The output format is dictionary with key as options and\
+              the value is a python list of the options")
+
+        edited_prompt = prompt_template.format(funnel_focus=funnel, user_quest=user_question)
+        print("prompt ........", edited_prompt)
+
+        response = self.conversation.predict(input=edited_prompt)
+        print("response ........", response)
+
+        return response
+
+
+
+    def generate_KPI(self, funnel, strategy, user_question):
+
+        # user_quest = str(json.load(inp)["user_quest"])
+        # mar_stages = json.load(inp)["stages"]
+
+        prompt_template = PromptTemplate.from_template("I am planning for a {strategy} marketing strategy to \
+            focus on {funnel_focus}.\n Generate a top 5 KPIs I should be analyzing for my {strategy} marketing \
+            strategy\n Give me just the list KPI names. Do not provide any explanation or introductions for the list of KPIs.\n \
+            The output format is dictionary with key as kpis and the value is a python list of the KPIs")
+        edited_prompt = prompt_template.format(funnel_focus=funnel, strategy= strategy)
+        print("prompt ........", edited_prompt)
+
+        response = self.conversation.predict(input=edited_prompt)
+        print("response ........", response)
+
+        return response
+
+    def generate_activity_theme(self,kpis, kpi_vals, comp):
+        prompt_template = PromptTemplate.from_template("Based on the strategy I choose earlier, \
+        goal I have choosen and the funnel focus. \n \
+        Here are the lists of {Kpi_list} and {Kpi_vals} . I want to achieve corresponding kpi values\
+         as an improvement within the strategy and focus. Given that my company is {company}. Give me a \
+         list of key product activity themes I can work on to achieve my goal and kpi improvement within my company.\n \
+         Activity theme is like a generic theme of solutions or like a category of solutions I can work on \
+         to acheive my goal within the choices I made to this point.")
+        edited_prompt = prompt_template.format(Kpi_list=kpis, Kpi_vals=kpi_vals, company = comp)
+        print("prompt ........", edited_prompt)
+
+        response = self.conversation.predict(input=edited_prompt)
+        print("response ........", response)
+
+        return response
+
+    def priscript_analy_recommendations(self, kpis, kpi_vals, theme, specs):
+        prompt_template = PromptTemplate.from_template("As explained earlier, I have this kpi list {Kpi_list} \
+         and corresponding kpi improvement values as such {Kpi_vals}. I want to select {activity_theme} \
+         from earlier activity themes list. Give me a list of specific Ideas I can implement within my company \
+         considering the theme, kpis and my goal and some additional specifications: {specifications}. \n \
+         Give me a list of prescriptive analyis and recommendations that I can work on to achieve my goal.\n \
+         The output format is a dictionary with keys prescriptive analysis, recommendations and Ideas \
+         and the values are key bullet points for prescriptive analysis, one line recommendations and a \
+         list of Ideas or Idea names.\n \
+         Donot provide any explanations, and the ideas can be catchy names inline with my company.")
+        edited_prompt = prompt_template.format(Kpi_list=kpis, Kpi_vals=kpi_vals, activity_theme=theme, specifications= specs)
+        print("prompt ........", edited_prompt)
+
+        response = self.conversation.predict(input=edited_prompt)
+        print("response ........", response)
+
+        return response
+
+    def idea_detail_view(self,user_feedback, ai_recom):
+        prompt_template = PromptTemplate.from_template("Based on my goal, strategy, kpis, activity theme, \
+        {usr_feed}, {ai_rec} and the list of Ideas given I want to go with the best idea among them.\n \
+        Give me the Target audience, a campaign Brief (list down my goal, strategy and all the choices \
+        I made in this process), production plan and Business case for the Idea.\n \
+        Give the output in previously defined output format.")
+        edited_prompt = prompt_template.format(usr_feed=user_feedback, ai_rec=ai_recom)
+        print("prompt ........", edited_prompt)
+
+        response = self.conversation.predict(input=edited_prompt)
+        print("response ........", response)
+
+        return response
+
+def main():
+
+    ContentLab.what_to_accomplish("How can I improve business?")
+    ContentLab.options_in_funnel_focus("awareness", "How can I improve business?")
+    ContentLab.generate_KPI("awareness","product_marketing", "How can I improve business?")
+    ContentLab.generate_activity_theme(["Website traffic", "Social media engagement", "Influencer reach"], ["50%", "50%", "50%"], "paypal")
+    ContentLab.priscript_analy_recommendations(["Website traffic", "Social media engagement", "Influencer reach"],
+                                       ["50%", "50%", "50%"], "Buy first Pay Later","Improve website traffic and Influencer reach")
+    ContentLab.idea_detail_view("PayPal PayLater Boost")
+
+if __name__ == '__main__':
+    print('ContentLab')
+    main()
---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, jsonify
+from Functions import *
+
+app = Flask(__name__)
+
+contentlab = ContentLab()
+
+variables = {}
+
+@app.route('/what_to_accomplish', methods=['POST'])
+def api_what_to_accomplish():
+    data = request.get_json()
+    user_question = data.get('user_question')  # Assuming the JSON contains 'user_question' field
+    variables['user_question']= user_question
+    if user_question:
+        output = contentlab.what_to_accomplish(user_question)
+        return jsonify({'message': 'Function executed successfully', "output": output})
+    else:
+        return jsonify({'error': 'Invalid data provided'}), 400
+
+@app.route('/options_in_funnel_focus', methods=['POST'])
+def api_options_in_funnel_focus():
+    data = request.get_json()
+    funnel = data.get('funnel')
+    user_question = variables["user_question"]
+    variables["funnel_focus"] = funnel
+    if funnel:
+        output = ContentLab.options_in_funnel_focus(funnel, user_question)
+        return jsonify({'message': 'Function executed successfully', "output": output})
+    else:
+        return jsonify({'error': 'Invalid data provided'}), 400
+
+@app.route('/generate_KPI', methods=['POST'])
+def api_generate_KPI():
+    data = request.get_json()
+    option = data.get('option')
+    funnel = variables["funnel_focus"]
+    user_question = variables["user_question"]
+    if option:
+        output = contentlab.generate_KPI(funnel, option, user_question)
+        return jsonify({'message': 'Function executed successfully', "output": output})
+    else:
+        return jsonify({'error': 'Invalid data provided'}), 400
+@app.route('/generate_activity_theme', methods=['POST'])
+def api_generate_activity_theme():
+    data = request.get_json()
+    KPI = data.get('kpi')
+    variables["kpi"] = KPI
+    values = data.get('values')
+    variables["kpi_vals"] = values
+    # company = data.get('paypal')  # hardcode company='paypal'
+    if KPI and values:
+        output = contentlab.generate_activity_theme(KPI,values,"paypal")
+        return jsonify({'message': 'Function executed successfully', "output": output})
+    else:
+        return jsonify({'error': 'Invalid data provided'}), 400
+@app.route('/prescrt_analy_recommendations', methods=['POST'])
+def api_prescrt_analy_recommendations():
+    data = request.get_json()
+
+    activity_theme = data.get('activity_theme')
+    KPI = variables["kpi"]
+    values = variables["kpi_vals"]
+    if activity_theme:
+        output = contentlab.priscript_analy_recommendations(KPI,values,activity_theme)
+        return jsonify({'message': 'Function executed successfully', "output": output})
+    else:
+        return jsonify({'error': 'Invalid data provided'}), 400
+
+@app.route('/idea_detail_view', methods=['POST'])
+def api_idea_detail_view():
+    data = request.get_json()
+    user_feedback = data.get('user_feedback')
+    ai_recom = data.get("ai_recommendation")
+
+    if user_feedback and ai_recom:
+        output = contentlab.idea_detail_view(user_feedback, ai_recom )
+        return jsonify({'message': 'Function executed successfully', "output": output})
+    else:
+        return jsonify({'error': 'Invalid data provided'}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
